19068cac934ce008ccb6d2e3bb8e7fc54cb099dd
songye38 <ddev0308@gmail.com>
2025-11-05 14:03:37 +0900

    UPDATE 콘솔로 찍어보기

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,7 +9,8 @@ import AnalyticsWrapper from './analytics/AnalyticsWrapper.tsx';
 
 // 배포 환경에서만 GA 초기화
 if (import.meta.env.VITE_ENV === 'production') {
-  ReactGA.initialize(import.meta.env.VITE_GA_ID);
+  ReactGA.initialize(import.meta.env.VITE_GA_ID as string);
+  console.log("vite ga id", import.meta.env.VITE_GA_ID)
 }
 
 // 루트 렌더링
@@ -21,4 +22,4 @@ createRoot(document.getElementById('root')!).render(
       </AnalyticsWrapper>
     </BrowserRouter>
   </StrictMode>,
-);
+)
